f96349653d010b5959f144b05b66f54f7280ac1f
Alexander Sysoev <alexander@acid.cl>
2015-01-09 19:15:49 -0300

    Fix setting field helper

--- app/helpers/admin/base_helper.rb
+++ app/helpers/admin/base_helper.rb
@@ -2,7 +2,16 @@ module Admin
 	module BaseHelper
 
 		def setting_field form, setting
-			form.input setting.name, as: setting.field_type, input_html: { class: 'form-control', value: setting.value }
+			options = { class: "form-control", value: setting.value, autocomplete: 'off' }
+
+			if setting.field_type == :boolean
+				options.delete(:value)
+				options[:checked] = setting.value
+			end
+
+			form.input setting.name, as: setting.field_type,
+					input_html: options
+
 		end
 
 	end

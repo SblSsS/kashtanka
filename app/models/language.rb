c973b64d571fbb1e95467924cd991cdc32de2a66
Alexander Sysoev <alexsblsss@gmail.com>
2015-01-10 18:49:33 -0300

    Remove ability to destroy default language

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,2 +1,14 @@
 class Language < ActiveRecord::Base
+
+	#Validations
+	validates :name, :iso, presence: true
+
+	#Methods-----------------------------
+
+	def destroy
+		super if iso != "en"
+	end
+
+	#------------------------------------
+
 end

3c22a93bb93ba53290a578fdd5badef9906c6c19
Alexander Sysoev <alexsblsss@gmail.com>
2015-01-10 18:29:04 -0300

    Add language specs and factories

--- spec/factories/languages.rb
+++ spec/factories/languages.rb
@@ -1,6 +1,17 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :language do
+  	name "English"
+  	iso  "en"
+  	published true
+
+  	factory :language_russian do
+  		name "Russian"
+  		iso  "ru"
+  	end
+
+  	factory :language_spanish do
+  		name "Spanish"
+  		iso  "es"
+  	end
   end
 end
--- spec/models/language_spec.rb
+++ spec/models/language_spec.rb
@@ -1,5 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe Language, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+	let(:english) { FactoryGirl.create(:language) }
+	let(:spanish) { FactoryGirl.create(:language_spanish) }
+
+	describe 'attributes' do
+
+		it 'has name' do
+			expect(subject).to respond_to(:name) 
+		end
+
+		it 'has iso' do
+			expect(subject).to respond_to(:iso) 
+		end
+
+		it 'has published' do
+			expect(subject).to respond_to(:published) 
+		end
+
+	end
+
+	describe 'validations' do
+
+		it 'has valid factory' do
+			expect(FactoryGirl.build(:language)).to be_valid
+		end
+
+		it 'is not valid without name' do
+			expect(FactoryGirl.build(:language, name: nil)).not_to be_valid
+		end
+
+		it 'is not valid without iso' do
+			expect(FactoryGirl.build(:language, iso: nil)).not_to be_valid
+		end
+
+	end
+
+	describe 'methods' do
+
+		it 'can not be destroyed if en' do
+			english.destroy
+			expect(Language.find_by(iso: english.iso)).not_to be_nil
+		end
+
+		it 'can be destroyed if not en' do
+			spanish.destroy
+			expect(Language.find_by(iso: spanish.iso)).to be_nil
+		end
+
+	end
+
 end

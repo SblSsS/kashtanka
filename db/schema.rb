f18a0cbc540b9ba767ed47abc18ca5a95028d6fd
Alexander Sysoev <alexsblsss@gmail.com>
2015-01-10 17:07:08 -0300

    Add language model with specs

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150106231803) do
+ActiveRecord::Schema.define(version: 20150110200122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "languages", force: true do |t|
+    t.string  "name"
+    t.string  "iso"
+    t.boolean "published"
+  end
 
   create_table "roles", force: true do |t|
     t.string "name"

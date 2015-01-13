1f6e96859cc776c71216c3265bce800670643472
Alexander Sysoev <alexsblsss@gmail.com>
2015-01-13 01:08:39 -0300

    Add helper for translation fieldset

--- app/helpers/admin/base_helper.rb
+++ app/helpers/admin/base_helper.rb
@@ -18,5 +18,26 @@ module Admin
 			resource.published ? "glyphicon glyphicon-eye-open publish" : "glyphicon glyphicon-eye-close unpublish"
 		end
 
+		def translation_fieldset form, field, object, index, iso, options={}
+			translation = object.translations.find_by(field_name: field, locale: iso)
+			prefix 			= "#{object.class.name.parameterize}[translations_attributes][#{index}]"
+
+			if translation
+				id 				= form.input :id, as: :hidden, input_html: {value: translation.id, name: "#{prefix}[id]"}
+			else
+				id 				= ''
+			end
+
+			name   			= form.input :field_name, as: :hidden, input_html: {value: field, name: "#{prefix}[field_name]"}
+			iso    			= form.input :locale, as: :hidden, input_html: { value: iso, name: "#{prefix}[locale]" }
+			value  			= form.input :field_translation, label: object.class.human_attribute_name(field),
+											as: options[:as] ? options[:as] : :string, required: false,
+											input_html: { value: translation ? translation.field_translation : '',
+											name: "#{prefix}[field_translation]", 
+											class: "form-control #{options[:class] ? options[:class] : ''}" }
+
+			(id + name + iso + value).html_safe
+		end
+
 	end
 end

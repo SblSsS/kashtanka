38355a716e9e7c75beac67bf93659425c45c88d9
Alexander Sysoev <alexander@acid.cl>
2015-01-13 14:30:27 -0300

    Add fields constant to model

--- app/models/concerns/translatable.rb
+++ app/models/concerns/translatable.rb
@@ -5,10 +5,15 @@ module Translatable
     has_many :translations, as: :translatable
     default_scope { includes(:translations) }
     accepts_nested_attributes_for :translations, reject_if: :invalid?
+
+    after_create :create_default_translation
   end
 
   module ClassMethods
     def acts_as_translatable *fields
+
+      self.table_name.singularize.camelize.constantize.const_set(:TRANSLATABLE_FIELDS, fields)
+
       fields.each do |f|
         define_method "#{f}" do
           if Settings.config[:multilanguage]
@@ -34,6 +39,17 @@ module Translatable
 
     def invalid?(attributed)
       attributed[:field_translation].blank?
+    end
+
+    def create_default_translation
+      default_locale = Language.published.map(&:iso).include?(I18n.locale.to_s) ?
+                       I18n.locale.to_s : Language.published.first.iso
+
+      self.class.const_get(:TRANSLATABLE_FIELDS).each do |field|
+        translations.create(field_name: field,
+                            field_translation: read_attribute(field.to_sym),
+                            locale: default_locale)
+      end
     end
 
 end

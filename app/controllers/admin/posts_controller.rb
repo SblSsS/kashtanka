81b8c6f9f0b808ac31865bc1ac9528f5b4f0f267
Alexander Sysoev <alexsblsss@gmail.com>
2015-01-25 21:46:00 -0300

    Add filter form with scopes and permited params

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -3,8 +3,20 @@ module Admin
 		
 		def index
 			super
-			@posts = @posts.order(created_at: :desc)
+			@posts = @posts.order('posts.created_at DESC')
 		end
+
+		private
+
+			def filter_params
+				params.slice(:by_title, :by_tags, :by_published, :by_date)
+			end
+
+			def resource_params
+				params.require(:post).permit(:title, :body, :published, :tags, :slug, 
+																		 post_image_attributes: [:id, :attachment],
+																		 translations_attributes: [:id,:field_name,:locale,:field_translation])
+			end
 
 	end
 end
